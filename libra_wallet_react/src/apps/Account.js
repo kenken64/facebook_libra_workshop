f99f4aef80b1cdc63ef7365b929d2e49401b92c2
Kenneth Phang <bunnyppl@gmail.com>
2019-07-26 15:42:41 +0800

    fix

--- libra_wallet_react/src/apps/Account.js
+++ libra_wallet_react/src/apps/Account.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { Component } from "react";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
@@ -8,106 +7,85 @@ import IconButton from "@material-ui/core/IconButton";
 import InfoIcon from "@material-ui/icons/Info";
 import { LIBRA_API_URL } from "./config";
 import firebase from "./Firestore";
-import lifecycle from "react-pure-lifecycle";
+import styles from "./Account.css";
+import axios from "axios";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.paper
-  },
-  gridList: {
-    width: 500,
-    height: "100%"
-  },
-  icon: {
-    color: "rgba(255, 255, 255, 0.54)"
+class Account extends Component {
+  constructor() {
+    console.log("constructor...");
+    super();
+    const db = firebase.firestore();
+    this.walletRef = db.collection("wallet");
+
+    this.state = {
+      libra_image: "https://bit.ly/2Gr7VSV",
+      accData: "",
+      list: [],
+      idx: 0
+    };
   }
-}));
 
-var accountArr = [];
+  componentWillMount() {
+    console.log("before render ..");
+  }
 
-async function renderAccounts() {
-  console.log("init ...fetch accounts from the libra test net");
-  const db = firebase.firestore();
-
-  const walletRef = db.collection("wallet");
-  await walletRef.get().then(querySnapshot => {
-    const data = querySnapshot.docs.map((doc, index) => {
-      fetch(`${LIBRA_API_URL}/balance/${doc.data().address}`, {
-        method: "get",
-        headers: { "Content-Type": "application/json" }
-      })
-        .then(function(response) {
-          console.log(response);
-          return response.json();
-        })
-        .then(function(data) {
-          console.log(index);
-          let accData = {
-            id: index,
-            address: doc.data().address,
-            balance: data.balance,
-            walletName: doc.data().walletName
-          };
-          console.log(accData.id);
-
-          return accData;
-        })
-        .catch(error => {
-          console.log(error);
+  componentDidMount() {
+    this.walletRef.get().then(querySnapshot => {
+      querySnapshot.forEach(async doc => {
+        const { walletName, address } = doc.data();
+        console.log("...id " + doc.id);
+        let res = await axios.get(`${LIBRA_API_URL}/balance/${address}`);
+        const account = res.data;
+        console.log(account);
+        console.log(this.state.idx);
+        let accData = {
+          id: doc.id,
+          address: address,
+          balance: account.balance,
+          walletName: walletName
+        };
+        this.setState({ accData: accData });
+        this.setState(state => {
+          state.list.push(state.accData);
         });
+        this.setState({ idx: this.state.idx++ });
+      });
     });
-    console.log(typeof data);
-    accountArr = data;
-  });
-}
+  }
 
-// create your lifecycle methods
-const componentDidMount = props => {
-  console.log("I mounted! Here are my props: ", props);
-  renderAccounts();
-};
-
-// make them properties on a standard object
-const methods = {
-  componentDidMount
-};
-
-function Account(props) {
-  const classes = useStyles();
-  const libra_image = "https://bit.ly/2Gr7VSV";
-  //const [accountList, setAccountList] = useState([]);
-  console.log(accountArr.length);
-
-  return (
-    <div className={classes.root}>
-      <GridList cellHeight={180} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
-          <ListSubheader component="div">Accounts</ListSubheader>
-        </GridListTile>
-        {accountArr.map(acc => (
-          <GridListTile key={acc.id}>
-            <img src={libra_image} alt={acc.walletName} />
-            <GridListTileBar
-              title={acc.walletName}
-              subtitle={<span>by: {acc.address}</span>}
-              actionIcon={
-                <IconButton
-                  aria-label={`info about ${acc.balance}`}
-                  className={classes.icon}
-                >
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
+  render() {
+    console.log("rendering ui ..");
+    return (
+      <div className={styles.root}>
+        <GridList cellHeight={180} className={styles.gridList}>
+          <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
+            <ListSubheader component="div">Accounts</ListSubheader>
           </GridListTile>
-        ))}
-      </GridList>
-    </div>
-  );
+          {this.state.list.map(acc => (
+            <GridListTile key={acc.id}>
+              <img
+                style={{ height: "200px", width: "200px" }}
+                src={this.state.libra_image}
+                alt={acc.walletName}
+              />
+              <GridListTileBar
+                title={acc.walletName}
+                subtitle={<span>Balance : {acc.balance}</span>}
+                actionIcon={
+                  <IconButton
+                    aria-label={`info about ${acc.address}`}
+                    className={styles.icon}
+                  >
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    );
+  }
 }
 
-export default lifecycle(methods)(Account);
+export default Account;
